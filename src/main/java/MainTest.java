09fe8c5ed80ca78bd3492a7e4ef64fc28ad4ee9c
JianweiWang <786594722@qq.com>
2014-08-23 15:04:50 +0800

    Initial commit

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by root on 14-8-23.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        MyMethods myMethods = new MyMethods("wjw","zhengji","jianwei.wjw");
+        myMethods.getName1();
+    }
+}
